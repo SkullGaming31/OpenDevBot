2d073bd805a324503c8459823dd4a1426c9724c6
skullgaming31 <canadianfan11@hotmail.com>
2024-03-01 07:16:04 -0330

    typed the userAuthProvider to RefreshingAuthProvider

--- src/api/userApiClient.ts
+++ src/api/userApiClient.ts
@@ -1,8 +1,9 @@
 import { ApiClient } from '@twurple/api';
+import { RefreshingAuthProvider } from '@twurple/auth';
 import { getAuthProvider } from '../auth/authProvider';
 
 export async function getUserApi(): Promise<ApiClient> {
-	const userAuthProvider = await getAuthProvider();
+	const userAuthProvider: RefreshingAuthProvider = await getAuthProvider();
 	const isProduction = process.env.Enviroment === 'prod';
 	const minLevel: 'ERROR' | 'INFO' | 'CRITICAL' | 'DEBUG' | 'WARNING' | 'TRACE' = isProduction ? 'DEBUG' : 'INFO';
 
