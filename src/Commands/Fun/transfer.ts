28b9a0041ffec86d8468053568e89a3297ef9c59
skullgaming31 <canadianfan11@hotmail.com>
2024-03-02 23:06:45 -0330

    :wrench: fixed gold not being transfered to the user

--- src/Commands/Fun/transfer.ts
+++ src/Commands/Fun/transfer.ts
@@ -1,4 +1,5 @@
 import { ChatMessage } from '@twurple/chat/lib';
+import { getUserApi } from '../../api/userApiClient';
 import { getChatClient } from '../../chat';
 import { User, UserModel } from '../../database/models/userModel';
 import { Command } from '../../interfaces/Command';
@@ -8,33 +9,50 @@ const transfer: Command = {
 	description: 'Transfer money to another person',
 	usage: '!transfer [@name] [amount]',
 	execute: async (channel: string, user: string, args: string[], text: string, msg: ChatMessage) => {
-		const chatClient = await getChatClient();
-		const [_, recipient, amount] = text.split(' ');
-		const sender = user.toLowerCase();
-		const parsedAmount = parseInt(amount, 10);
+		try {
+			const chatClient = await getChatClient();
+			const userAPIClient = await getUserApi();
+			const [_, recipient, amount] = text.split(' ');
+			const sender = user.toLowerCase();
+			const parsedAmount = parseInt(amount, 10);
 
-		if (!args[0]) return chatClient.say(channel, `Usage: ${transfer.usage}`);
-		if (isNaN(parsedAmount)) return chatClient.say(channel, `@${user}, please specify a valid amount.`);
-		if (parsedAmount <= 0) return chatClient.say(channel, `@${user}, you can only transfer positive amounts.`);
+			const userSearch = userAPIClient.users.getUserByName(recipient.substring(1));
+			if (userSearch === undefined) return;
 
-		const senderDoc = await UserModel.findOne<User>({ username: sender });
-		if (senderDoc?.balance === undefined) return;
-		if (!senderDoc || senderDoc.balance < parsedAmount) {
-			return chatClient.say(channel, `@${user}, you do not have enough gold to make this transfer.`);
+			if (!args[0]) return chatClient.say(channel, `Usage: ${transfer.usage}`);
+			if (isNaN(parsedAmount)) return chatClient.say(channel, `@${user}, please specify a valid amount.`);
+			if (parsedAmount <= 0) return chatClient.say(channel, `@${user}, you can only transfer positive amounts.`);
+
+			// Combine sender balance check and update into a single operation
+			const updatedSenderDoc = await UserModel.findOneAndUpdate<User>(
+				{ username: sender.toLowerCase() },
+				{
+					$inc: { balance: -parsedAmount },// Decrement balance directly
+				},
+				{ new: true } // Return updated document for checks
+			);
+			if (updatedSenderDoc?.balance === undefined) return;
+
+			// Handle potential errors with sender balance
+			if (!updatedSenderDoc) {
+				return chatClient.say(channel, `@${user}, you don't have a gold balance.`);
+			}
+			if (updatedSenderDoc.balance < 0) { // Ensure balance is not negative
+				return chatClient.say(channel, `@${user}, you don't have enough gold to make this transfer.`);
+			}
+
+			const recipientDoc = await UserModel.findOneAndUpdate<User>({ username: recipient.toLowerCase() });
+			if (recipientDoc && recipientDoc.balance !== undefined) {
+				recipientDoc.balance += parsedAmount;
+				recipientDoc.save(); // Updates the saved document
+				await chatClient.say(channel, `@${user}, you have transferred ${parsedAmount} gold to ${recipientDoc.username}.`);
+			} else {
+				// Handle recipient not found
+			}
+
+		} catch (error) {
+			console.error(error);
 		}
-
-		const recipientDoc = await UserModel.findOneAndUpdate<User>(
-			{ username: recipient.toLowerCase() },
-			{ $inc: { balance: parsedAmount } },
-			{ new: true, upsert: true }
-		);
-
-		await UserModel.updateOne<User>(
-			{ username: sender },
-			{ $inc: { balance: -parsedAmount } }
-		);
-
-		chatClient.say(channel, `@${user}, you have transferred ${parsedAmount} gold to ${recipientDoc.username}.`);
 	}
 };
 export default transfer;
