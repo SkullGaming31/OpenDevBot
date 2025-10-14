c85ea93bf3d7d2828f047e9f7bcb5279545a253e
SkullGamingHQ <skullgamingg31@gmail.com>
2025-10-13 21:33:10 -0230

    removed dashboard
    removed the dashboard to do in another project down the road

--- web/next.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-    reactStrictMode: true,
-    typescript: {
-        ignoreBuildErrors: false
-    }
-};
-
-module.exports = nextConfig;
